a56e96e8572902126e0fba0f2c952e877f7d4941
grodriguez495 <144068996+grodriguez495@users.noreply.github.com>
2024-01-18 14:18:15 -0500

    Add sensor unit test

---
+++ AirQualityControlAPI/AirQualityControlUnitTest/Sensors/GetSensorByDateAndVariableQueryHandlerTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using AirQualityControlAPI.Application.Features.Sensors.Queries;
+using AirQualityControlAPI.Application.Features.Sensors.Queries.GetSensorValuesBydatesAndVariables;
+using AirQualityControlAPI.Domain.Models;
+using AirQualityControlAPI.Domain.Repositories.Sensors.Queries;
+using AutoMapper;
+using Moq;
+
+namespace AirQualityControlUnitTest.Sensors;
+[TestClass]
+public class GetSensorByDateAndVariableQueryHandlerTest
+{
+    private readonly ISensorQueryRepository _sensorQueryRepository;
+    private readonly IMapper _mapper;
+    private readonly GetSensorValuesByDatesAndVariablesQueryHandler _getSensorValuesByDatesAndVariablesQueryHandler;
+
+    public GetSensorByDateAndVariableQueryHandlerTest()
+    {
+        _sensorQueryRepository = Mock.Of<ISensorQueryRepository>(MockBehavior.Strict);
+        _mapper = Mock.Of<IMapper>(MockBehavior.Strict);
+        _getSensorValuesByDatesAndVariablesQueryHandler =
+            new GetSensorValuesByDatesAndVariablesQueryHandler(_sensorQueryRepository, _mapper);
+    }
+
+    [TestMethod]
+    public async Task GivenSensorRequest_ReturnSensorList()
+    {
+        var sensor = new Sensor()
+        {
+            SensorId = "test",
+            Timestamp = DateTime.Now,
+            Latitud = 2,
+            Longitud = 2,
+            VariableId = 1,
+            Value = "1234"
+        };
+        var sensorDto = new SensorDto();
+        var queryCommand = new GetSensorValuesByDatesAndVariablesQuery(DateTime.Now.ToString(),DateTime.Now.ToString(),It.IsAny<int>()) { };
+        Mock.Get(_sensorQueryRepository)
+            .Setup(x => x.ListAsync(
+                It.IsAny<Expression<Func<Sensor, bool>>>(),
+                It.IsAny<bool>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<Sensor>() { sensor })
+            .Verifiable();
+        Mock.Get(_mapper)
+            .Setup(x => x.Map<List<SensorDto>>(new List<Sensor>() { sensor }))
+            .Returns(new List<SensorDto>() { sensorDto })
+            .Verifiable();
+        var response =
+            await _getSensorValuesByDatesAndVariablesQueryHandler.Handle(queryCommand, CancellationToken.None);
+        Assert.IsNotNull(response);
+    }
+}
---
+++ AirQualityControlAPI/AirQualityControlUnitTest/Sensors/GetSensorVariablesQueryHandlerTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+using AirQualityControlAPI.Application.Features.Sensors.Queries;
+using AirQualityControlAPI.Application.Features.Sensors.Queries.GetSensors;
+using AirQualityControlAPI.Domain.Models;
+using AirQualityControlAPI.Domain.Repositories.Sensors.Queries;
+using AutoMapper;
+using Moq;
+
+namespace AirQualityControlUnitTest.Sensors;
+
+[TestClass]
+public class GetSensorVariablesQueryHandlerTest
+{
+    private readonly ISensorQueryRepository _sensorQueryRepository;
+    private readonly IMapper _mapper;
+    private readonly GetSensorVariablesQueryHandler _getSensorVariablesQueryHandler;
+
+    public GetSensorVariablesQueryHandlerTest()
+    {
+        _sensorQueryRepository = Mock.Of<ISensorQueryRepository>(MockBehavior.Strict);
+        _mapper = Mock.Of<IMapper>(MockBehavior.Strict);
+        _getSensorVariablesQueryHandler = new GetSensorVariablesQueryHandler(_sensorQueryRepository, _mapper);
+    }
+
+    [TestMethod]
+    public async Task GivensensorRequest_ReturnSensorList()
+    {
+        var sensor = new Sensor()
+        {
+            SensorId = "test",
+            Timestamp = DateTime.Now,
+            Latitud = 2,
+            Longitud = 2,
+            VariableId = 1,
+            Value = "1234"
+        };
+        var sensorDto = new SensorDto();
+        Mock.Get(_sensorQueryRepository)
+            .Setup(x => x.ListAsync(
+                It.IsAny<Expression<Func<Sensor, bool>>>(),
+                It.IsAny<bool>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<Sensor>() { sensor })
+            .Verifiable();
+        Mock.Get(_mapper)
+            .Setup(x => x.Map<List<SensorDto>>(new List<Sensor>() { sensor }))
+            .Returns(new List<SensorDto>() { sensorDto })
+            .Verifiable();
+        var response =
+            await _getSensorVariablesQueryHandler.Handle(new GetSensorVariablesQuery(), CancellationToken.None);
+        Assert.IsNotNull(response);
+    }
+}
